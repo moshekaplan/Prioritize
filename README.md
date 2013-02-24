41aeacd552a45b04e84fc708934cb584b331a9ef
Moshe Kaplan <mk.moshe.kaplan@gmail.com>
2013-02-24 00:47:42 -0500

    Added DB

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 Prioritize-Data
 ===============
 
-Takes jpeg images obtained by file-carving and extracts features from them to help prioritize "interesting" images.
+Takes files obtained by file-carving and extracts features from them to help prioritize "interesting" files.
+
+One of the problems related to forensics is dealing with the sheer amount of data that can be retrieved from a system. The purpose of this project is to determine which files would likely be the most interesting for an investigator. This will be done by extracting features that would be deemed relevant for an investigation.
+
+As a proof of concept, it will contain a module for determining which JPEG images are interesting. This will be done by checking the structure of the image, the variation of colors within the image, if the image contains EXIF data, if the image contains a person's face, and if there is reason to believe that the image contains a screenshot of a person's desktop.
--- jpeg_parser.py
+++ jpeg_parser.py
@@ -11,13 +11,21 @@ Write some scripts for prioritizing files based on some combination of these fea
 
 First table:
 
-ID / Path / File Size / MD5 / SHA2-512 / Well-formed / 
+ID / Path / File Size / MD5 / SHA2-512
+
+JPEG Table:
+
+Well-formed / contains face / possible screenshot (contains a known icon) / normal distribution of colors (or perhaps some number) / EXIF data / common EXIF fields
+
 
 """
 
 import os
+import sys
+import time
 import hashlib
 import os.path
+import sqlite3
 
 
 # PIL
@@ -48,17 +56,75 @@ def get_file_list(rootdir, maxfiles=None):
 
 def get_hashes(fname):
   """Returns the MD5 and SHA-512 hashes for a file"""
-  pass
+  with open(fname,'rb') as fh:
+    data = fh.read()
+    md5 = hashlib.md5(data).hexdigest()
+    sha512 = hashlib.sha512(data).hexdigest()
+    return md5, sha512
   
-
+def load_cascades():
+  fnames = ['./haarcascades/haarcascade_frontalface_alt.xml',
+            './haarcascades/haarcascade_frontalface_alt2.xml']
+  
+  cascades= []
+  
+  for cascade_fn in fnames:
+    if os.path.exists(cascade_fn) and os.path.isfile(cascade_fn):
+      cascade = cv2.CascadeClassifier(cascade_fn)
+      cascades.append(cascade)
+  if not cascades:
+    print "Failed to load any classifier!"
+    sys.exit(1)
+  
+  return cascades
+  
 ###############################################################################
-# Database
+# Database-related functionality
 ###############################################################################
 
-def create_db():
+# Database filename
+DB_NAME = "prioritize_data.sqlite"
+
+# Create table statements
+CREATE_FILES_TABLE_QUERY = '''CREATE TABLE IF NOT EXISTS files (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  filename TEXT,
+  filesize INTEGER,
+  md5 TEXT,
+  sha512 TEXT
+  )'''
+
+# Create table for JPEG results
+CREATE_JPEG_TABLE_QUERY = '''CREATE TABLE IF NOT EXISTS jpeg (
+  file_id INTEGER,
+  well_formed BOOLEAN,
+  contains_face BOOLEAN
+  )'''
+
+# Insert statements
+
+INSERT_FILE_QUERY = '''INSERT INTO files (filename,filesize,md5,sha512) VALUES (?, ?, ?, ?)'''
+
+INSERT_JPEG_QUERY = '''INSERT INTO jpeg
+  (file_id, well_formed, contains_face) VALUES (?, ?, ?)'''
+           
+def create_db(cursor):
+  cursor.execute(CREATE_FILES_TABLE_QUERY)
+  cursor.execute(CREATE_JPEG_TABLE_QUERY)
   pass
 
+def close_db(conn):
+  conn.commit()
+  conn.close()
 
+def insert_file_entry(cursor, filename, filesize, md5, sha512):
+  cursor.execute(INSERT_FILE_QUERY, (filename, filesize, md5, sha512))
+  return cursor.lastrowid
+  
+def insert_jpeg_entry(cursor, fileid, well_formed, contains_face):
+  cursor.execute(INSERT_JPEG_QUERY, (fileid, well_formed, contains_face))
+
+  
 ###############################################################################
 # Image processing
 ###############################################################################
@@ -66,11 +132,6 @@ def create_db():
 def load_image(fname):
   img = cv2.imread(fname)
   return img
-
-def load_cascade(fname):
-  """Given a filename, returns the loaded cascade"""
-  cascade = cv2.CascadeClassifier(fname)
-  return cascade
 
 def is_well_structured(filename):
   try:
@@ -87,7 +148,7 @@ def is_well_structured(filename):
     return False
 
   
-def contains_face(img, cascade):
+def has_face(img, cascades):
   """\
   Magic from https://github.com/Itseez/opencv/blob/master/samples/python2/facedetect.py
   Takes a file loaded with cv2.imread(fname) and runs a cascade through it.
@@ -95,30 +156,76 @@ def contains_face(img, cascade):
   """
   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   gray = cv2.equalizeHist(gray)
-  rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
-  if len(rects):
-    return True
-  else:
-    return False
+  # Returns True if a single cascade matches
+  for cascade in cascades:
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
+    if len(rects):
+      return True
+  return False
 
 def contains_flesh():
   pass
 
 
+def process_jpeg(cascades, cursor, file_id, fname):
+  """Do all of the work required to process a single JPEG"""
+
+  well_structured = False
+  contains_face = False
+
+  well_structured = is_well_structured(fname)
+  if well_structured:
+    img = load_image(fname)
+    contains_face = has_face(img, cascades)
+  
+  insert_jpeg_entry(cursor, file_id, well_structured, contains_face)
+  
+
 ###############################################################################
 # Tie everything up!
 ###############################################################################
 
+def process_file(cascades, cursor, fname):
+  """This is the function responsible for tying together all of the other parsing modules"""
+  
+  # First do the minimal amount we do for every file
+  md5, sha512 = get_hashes(fname)
+  size = os.path.getsize(fname)
+  file_id = insert_file_entry(cursor, fname, size, md5, sha512)
+
+  # Then handle the remaining modules  
+  process_jpeg(cascades, cursor, file_id, fname)
+  
+
+def main():
+
+  # Load up the classifiers
+  cascades = load_cascades()
+  
+  # Open a connection to the database and create it if necessary
+  conn = sqlite3.connect(DB_NAME)
+  cursor = conn.cursor()
+  create_db(cursor)
+  
+  start_time = time.time()
+  
+  # Get the list of JPEG files to process
+  files = get_file_list('/media/1.5TB/40_gb')
+  
+  file_time = time.time() - start_time
+  
+  # Process each of them
+  for fname in files:
+    result = process_file(cascades, cursor, fname)
+
+  processing_time = time.time() - file_time - start_time  
+  
+  close_db(conn)
+
+  if files:
+    print "Processed %d files in %0.3f seconds, for an average of %0.3f seconds/file" % (len(files),processing_time, processing_time/len(files))
+  else:
+    print "No files processed!"
 
 if __name__ =="__main__":
-  cascade_fn = "./haarcascades/haarcascade_frontalface_alt2.xml"
-  cascade = load_cascade(cascade_fn)  
-    
-  files = get_file_list('.')
-  for fname in files:
-    well_structured = is_well_structured(fname)
-    if well_structured:
-      img = load_image(fname)
-      face = contains_face(img, cascade)
-      if face:
-        print fname, is_well_structured(fname), face
+  main()
