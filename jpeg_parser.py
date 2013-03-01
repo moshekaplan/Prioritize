dbb83e89ee0469a8b0522027994c15d34bc5fc27
Moshe Kaplan <mk.moshe.kaplan@gmail.com>
2013-03-01 02:21:43 -0500

    Cleaned up the database, added some queries, added command line parsing

--- jpeg_parser.py
+++ jpeg_parser.py
@@ -15,7 +15,7 @@ ID / Path / File Size / MD5 / SHA2-512
 
 JPEG Table:
 
-Well-formed / contains face / possible screenshot (contains a known icon) / normal distribution of colors (or perhaps some number) / EXIF data / common EXIF fields
+Well-formed / number of faces / possible screenshot (contains a known icon) / normal distribution of colors (or perhaps some number) / EXIF data / common EXIF fields
 
 
 """
@@ -26,12 +26,13 @@ import time
 import hashlib
 import os.path
 import sqlite3
+import argparse
 
 
 # PIL
 import Image
 
-# OpenCV
+# OpenCV-
 import cv2
 import cv2.cv as cv
 
@@ -83,7 +84,7 @@ def load_cascades():
 ###############################################################################
 
 # Database filename
-DB_NAME = "prioritize_data.sqlite"
+DEFAULT_DB_NAME = "prioritize_data.sqlite"
 
 # Create table statements
 CREATE_FILES_TABLE_QUERY = '''CREATE TABLE IF NOT EXISTS files (
@@ -97,8 +98,8 @@ CREATE_FILES_TABLE_QUERY = '''CREATE TABLE IF NOT EXISTS files (
 # Create table for JPEG results
 CREATE_JPEG_TABLE_QUERY = '''CREATE TABLE IF NOT EXISTS jpeg (
   file_id INTEGER,
-  well_formed BOOLEAN,
-  contains_face BOOLEAN
+  faces INTEGER,
+  well_formed BOOLEAN
   )'''
 
 # Insert statements
@@ -106,7 +107,10 @@ CREATE_JPEG_TABLE_QUERY = '''CREATE TABLE IF NOT EXISTS jpeg (
 INSERT_FILE_QUERY = '''INSERT INTO files (filename,filesize,md5,sha512) VALUES (?, ?, ?, ?)'''
 
 INSERT_JPEG_QUERY = '''INSERT INTO jpeg
-  (file_id, well_formed, contains_face) VALUES (?, ?, ?)'''
+  (file_id, well_formed, faces) VALUES (?, ?, ?)'''
+
+# SELECT
+SELECT_SHA512_QUERY = '''SELECT sha512 FROM files WHERE sha512=? LIMIT 1'''
            
 def create_db(cursor):
   cursor.execute(CREATE_FILES_TABLE_QUERY)
@@ -124,10 +128,52 @@ def insert_file_entry(cursor, filename, filesize, md5, sha512):
 def insert_jpeg_entry(cursor, fileid, well_formed, contains_face):
   cursor.execute(INSERT_JPEG_QUERY, (fileid, well_formed, contains_face))
 
+def find_sha512(cursor, sha512):
+  result = cursor.execute(SELECT_SHA512_QUERY, (sha512,))
+  return result.fetchone()
   
 ###############################################################################
 # Image processing
 ###############################################################################
+
+SCREENSHOT_DIR = "./common_desktop_icons"
+
+def init_jpeg():
+    """ Loads in global variables for efficiency purposes"""
+    global g_cascades
+    global g_icons
+    
+    # Load in all of the classifiers
+    g_cascades = load_cascades()
+    
+    fnames = []
+    dirs = []
+    for entry in os.listdir(SCREENSHOT_DIR):
+        entry = os.path.join(SCREENSHOT_DIR, entry)
+        if os.path.isdir(entry):
+            dirs.append(entry)
+        elif os.path.isfile(entry):
+            fnames.append(entry)
+        else:
+            print fnames, dirs
+            raise Exception("What is '%s'?" % entry)
+    
+    g_icons = {}
+    # Load in all of the desktop icons:
+    # First the 'general' icons
+    g_icons['general'] = []
+    
+    for fname in fnames:
+        icon = cv2.imread(fname)
+        g_icons['general'].append(icon)
+    
+    # Then the OS-specific ones
+    for dir in dirs:
+        g_icons[dir] = []
+        for fname in os.listdir(os.path.join(SCREENSHOT_DIR, entry)):
+            icon = cv2.imread(fname)
+            g_icons[dir].append(icon)
+    
 
 def load_image(fname):
   img = cv2.imread(fname)
@@ -148,82 +194,181 @@ def is_well_structured(filename):
     return False
 
   
-def has_face(img, cascades):
+def get_num_faces(img):
   """\
   Magic from https://github.com/Itseez/opencv/blob/master/samples/python2/facedetect.py
-  Takes a file loaded with cv2.imread(fname) and runs a cascade through it.
+  Takes a file loaded with cv2.imread(fname) and runs a haarcascade through it.
   If it detects anything, we're good!
   """
   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   gray = cv2.equalizeHist(gray)
-  # Returns True if a single cascade matches
-  for cascade in cascades:
+
+  max_faces = 0
+  # Returns the largest amount of faces matched by a single cascade
+
+  for cascade in g_cascades:
     rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
-    if len(rects):
-      return True
-  return False
+    max_faces = max(max_faces, len(rects))    
+  return max_faces
 
-def contains_flesh():
-  pass
+#def contains_flesh():
+#  pass
 
 
-def process_jpeg(cascades, cursor, file_id, fname):
+def is_screenshot(img):
+    """\
+    Checks if the supplied image is likely to be a screenshot.
+    
+    This check is done by searching for a series of common icons.
+    Additionally, it will attempt to make a guess about the OS based on the
+    amount of matches of OS-specific images.
+    """
+
+    method = cv.CV_TM_SQDIFF_NORMED
+    
+    # Note: Not implemented yet!
+    return
+    "THIS IS BROKEN"
+    results = {}
+    results['general'] = 0
+    for fname in fnames:
+        icon = cv2.imread(fname)
+        print cv2.matchTemplate(icon, img, method)
+        results['general'] += 1
+    
+    for dir in dirs:
+        results[dir] = 0
+        for fname in os.listdir(os.path.join(SCREENSHOT_DIR, entry)):
+            icon = cv2.imread(fname)
+            print cv2.matchTemplate(icon, img, method)
+            results[dir] += 1
+    
+    # Now that we have general resulsts, let's compare them:
+    pass
+
+    
+
+def process_jpeg(cursor, file_id, fname):
   """Do all of the work required to process a single JPEG"""
 
   well_structured = False
-  contains_face = False
+  faces = 0
 
   well_structured = is_well_structured(fname)
   if well_structured:
     img = load_image(fname)
-    contains_face = has_face(img, cascades)
-  
-  insert_jpeg_entry(cursor, file_id, well_structured, contains_face)
+    faces = get_num_faces(img)
+
+  if g_debug:
+    print "Valid: %s, faces: %d" % (str(well_structured), faces)
+
+  insert_jpeg_entry(cursor, file_id, well_structured, faces)
+  return well_structured
   
 
 ###############################################################################
 # Tie everything up!
 ###############################################################################
 
-def process_file(cascades, cursor, fname):
+def process_file(cursor, fname):
   """This is the function responsible for tying together all of the other parsing modules"""
   
   # First do the minimal amount we do for every file
   md5, sha512 = get_hashes(fname)
   size = os.path.getsize(fname)
+  
+  # If it's already in the DB, no processing is necessary
+  if find_sha512(cursor, sha512):
+    if g_debug:
+      print "It's a duplicate! Skipped!"
+    return "duplicate"
+  
   file_id = insert_file_entry(cursor, fname, size, md5, sha512)
 
   # Then handle the remaining modules  
-  process_jpeg(cascades, cursor, file_id, fname)
+  valid = process_jpeg(cursor, file_id, fname)
+  return valid
   
+def build_argparser():
+  parser = argparse.ArgumentParser(description='Extracts features for prioritizing recovered data')
+  # g_debug mode
+  parser.add_argument('--debug', dest='debug', action='store_true',
+                      help='Add additional logging data')
+  
+  # DB filename  
+  parser.add_argument('--db', dest='db', action='store',
+                      default=DEFAULT_DB_NAME,
+                      help='Override the default filename used for the database (default is %s)' % DEFAULT_DB_NAME)
+  # maxfiles     
+  parser.add_argument('--maxfiles', dest='maxfiles', action='store',
+                      default=None,
+                      help='Specify an upper limit to the amount of files that are examined')
+             
+  # Path to examine (required)
+  parser.add_argument(dest='path', help='The root directory of the files to examine')
+  return parser
 
 def main():
-
-  # Load up the classifiers
-  cascades = load_cascades()
+  global g_debug
+  # First the initial argument parsing
+  parser = build_argparser()
+  args = parser.parse_args(sys.argv[1:])
+  
+  g_debug   = args.debug
+  db_name   = args.db
+  maxfiles  = args.maxfiles
+  path      = args.path
+  
+  
+  # Initialize stored data used for parsing JPEG files
+  init_jpeg()
   
   # Open a connection to the database and create it if necessary
-  conn = sqlite3.connect(DB_NAME)
+  if g_debug:
+    print "Connecting to DB: '%s'" % db_name
+  conn = sqlite3.connect(db_name)
   cursor = conn.cursor()
   create_db(cursor)
   
   start_time = time.time()
   
   # Get the list of JPEG files to process
-  files = get_file_list('/media/1.5TB/40_gb')
+  files = get_file_list(path, maxfiles)
+  if g_debug:
+    print "A list of %d files were retrieved" % len(files)
   
   file_time = time.time() - start_time
   
+  statistics = {}
+  statistics['valid'] = 0
+  statistics['invalid'] = 0
+  statistics['duplicates']   = 0
   # Process each of them
-  for fname in files:
-    result = process_file(cascades, cursor, fname)
+  for i, fname in enumerate(files):
+    if g_debug:
+      print "Processing file %d/%d : %s" % (i+1, len(files), fname)
+    result = process_file(cursor, fname)
+    if result == "duplicate":
+      statistics['duplicates'] += 1
+    elif result is True:
+      statistics['valid'] += 1
+    elif result is False:
+      statistics['invalid'] += 1
+    else:
+      raise Exception("Unexpected result for %s" % fname)
 
   processing_time = time.time() - file_time - start_time  
   
   close_db(conn)
 
+  print "*"*80
+  print "Statistics"
   if files:
     print "Processed %d files in %0.3f seconds, for an average of %0.3f seconds/file" % (len(files),processing_time, processing_time/len(files))
+    
+    print "%d/%d (%0.3f%%) files were duplicates" % (statistics['duplicates'], len(files), statistics['invalid']*100.0/len(files))
+    print "%d/%d (%0.3f%%) files were valid"   % (statistics['valid'], len(files), statistics['valid']*100.0/len(files))
+    print "%d/%d (%0.3f%%) files were invalid" % (statistics['invalid'], len(files), statistics['invalid']*100.0/len(files))
   else:
     print "No files processed!"
 
